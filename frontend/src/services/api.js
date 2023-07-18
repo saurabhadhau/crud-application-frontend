889bfea08647e4652a08f0d663db2110eadd980c
saurabh <saurabh@gmail.com>
2023-07-18 16:24:25 +0530

    sss

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://http://3.39.223.100/',
+    baseURL: 'http://3.39.223.100/',
     headers: { 
         "Content-Type": "application/x-www-form-urlencoded"
     }
